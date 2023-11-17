9310e960c307fae59a09688d0baed42f2f51d615
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2023-11-17 22:54:07 +0800

    add files and routing

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/get_form')
+def get_form():
+    return render_template('get_form.html')
+
+@app.route('/hi')
+def hi():
+    return render_template('hi.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-

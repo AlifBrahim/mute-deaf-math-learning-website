e87558bad1c7e792537a5be7d530207aef798009
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2023-12-19 17:36:51 +0800

    Fix sidebar not resizing with the sticky bar. Gave up using block function of Flask, I just put the same code but different content on every page

--- app.py
+++ app.py
@@ -2,36 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
 
-
 @app.route('/multiplication')
 def multiplication():  # put application's code here
-    return render_template('multiplication.html')
-
+    return render_template('multiplication.html', title='Multiplication')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
-
+    return render_template('contact.html', title='Contact')
 
 @app.route('/get_form')
 def get_form():
-    return render_template('get_form.html')
-
+    return render_template('get_form.html', title='Get Form')
 
 @app.route('/hi')
 def hi():
-    return render_template('hi.html')
-
+    return render_template('hi.html', title='Hi')
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
-
+    return render_template('result.html', title='Result')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOM fully loaded and parsed');
     window.onscroll = function() {myFunction()};
 
     var navbar = document.getElementById("navbar");
+    console.log('navbar:', navbar);
+
+    var sidebar = document.querySelector('.sidebar');
+    console.log('sidebar:', sidebar);
 
     var sticky = navbar.offsetTop;
 
     function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-        document.querySelector('.sidebar').style.marginTop = "60px"; /* Move the sidebar down */
-      } else {
-        navbar.classList.remove("sticky");
-        document.querySelector('.sidebar').style.marginTop = "0"; /* Move the sidebar up */
-      }
+        console.log('onscroll event fired');
+        if (window.pageYOffset >= sticky) {
+            console.log('Adding sticky class to navbar');
+            navbar.classList.add("sticky");
+            sidebar.style.top = "50px"; /* Move the sidebar down */
+        } else {
+            console.log('Removing sticky class from navbar');
+            navbar.classList.remove("sticky");
+            sidebar.style.top = "0"; /* Move the sidebar up */
+        }
     }
 });

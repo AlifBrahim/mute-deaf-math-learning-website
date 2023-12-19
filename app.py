f1fc083467576aedb9a75c7223bf87ef1f1fc023
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2023-12-19 22:07:33 +0800

    Modify the names to syllabus names

--- app.py
+++ app.py
@@ -2,29 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
+
 
 @app.route('/multiplication')
 def multiplication():  # put application's code here
     return render_template('multiplication.html', title='Multiplication')
 
+
+@app.route('/darab-2-digit')
+def darab2digit():  # put application's code here
+    return render_template('darab/darab-2-digit.html', title='Darab 2 Digit')
+
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
+
 
 @app.route('/get_form')
 def get_form():
     return render_template('get_form.html', title='Get Form')
 
+
 @app.route('/hi')
 def hi():
     return render_template('hi.html', title='Hi')
 
+
 @app.route('/result')
 def result():
     return render_template('result.html', title='Result')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
